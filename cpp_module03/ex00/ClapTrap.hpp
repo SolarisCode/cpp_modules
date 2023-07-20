22e8348c4eee62a4b076d6fa7eac19042c3047b1
melkholy <28461793+SolarisCode@users.noreply.github.com>
2023-07-20 16:16:07 +0200

    Adding setters and getters for ClapTrap class

--- cpp_module03/ex00/ClapTrap.hpp
+++ cpp_module03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: melkholy <melkholy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 15:44:19 by melkholy          #+#    #+#             */
-/*   Updated: 2023/07/20 15:54:39 by melkholy         ###   ########.fr       */
+/*   Updated: 2023/07/20 16:15:19 by melkholy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,17 @@ public:
 	ClapTrap(std::string name);
 	ClapTrap(ClapTrap const& src);
 	ClapTrap&	operator=(ClapTrap const& rhs);
-	void	attack(const std::string& target);
-	void	takeDamage(unsigned int amount);
-	void	beRepaired(unsigned int amount);
+	void		attack(const std::string& target);
+	void		takeDamage(unsigned int amount);
+	void		beRepaired(unsigned int amount);
+	std::string	getName(void) const;
+	int			getHitPoints(void) const;
+	int			getEnergyPoints(void) const;
+	int			getAttackDamage(void) const;
+	void		setName(std::string newName);
+	void		setHitPoints(int hitPoints);
+	void		setEnergyPoints(int energyPoints);
+	void		setAttackPoints(int attackPoints);
 	~ClapTrap(void);
 
 private:
