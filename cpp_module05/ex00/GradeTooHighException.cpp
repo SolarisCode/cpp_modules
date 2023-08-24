0e9db0c86bf6bd4cd6824bcdaf92f4fec0418066
Mohammed Elkholy <28461793+SolarisCode@users.noreply.github.com>
2023-08-24 22:30:08 +0200

    GradeTooHighException class was modified

--- cpp_module05/ex00/GradeTooHighException.cpp
+++ cpp_module05/ex00/GradeTooHighException.cpp
@@ -6,12 +6,23 @@
 /*   By: melkholy <melkholy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 18:10:30 by melkholy          #+#    #+#             */
-/*   Updated: 2023/07/24 18:12:31 by melkholy         ###   ########.fr       */
+/*   Updated: 2023/08/24 21:06:07 by melkholy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "GradeTooHighException.hpp"
 
+GradeTooHighException::GradeTooHighException(std::string const& message)
+	: _message(message)
+{
+	this->_message.append("::GradeTooHighException");
+	return;
+}
+
 const char*	GradeTooHighException::what(void) const throw() {
-	return ("Bureaucrat::GradeTooHighException");
+	return (this->_message.c_str());
+}
+
+GradeTooHighException::~GradeTooHighException(void) _NOEXCEPT {
+	return;
 }
--- cpp_module05/ex00/GradeTooHighException.hpp
+++ cpp_module05/ex00/GradeTooHighException.hpp
@@ -6,7 +6,7 @@
 /*   By: melkholy <melkholy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 18:05:17 by melkholy          #+#    #+#             */
-/*   Updated: 2023/07/24 18:14:12 by melkholy         ###   ########.fr       */
+/*   Updated: 2023/08/24 20:05:12 by melkholy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,12 @@
 class	GradeTooHighException: public std::exception
 {
 public:
+	GradeTooHighException(std::string const& message);
 	virtual const char* what(void) const throw();
+	~GradeTooHighException(void) _NOEXCEPT;
 
+private:
+	std::string	_message;
 };
 
 #endif
