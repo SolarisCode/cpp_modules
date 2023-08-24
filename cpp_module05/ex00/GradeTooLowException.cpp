08458290054571ffa6514481cb40664280aec326
Mohammed Elkholy <28461793+SolarisCode@users.noreply.github.com>
2023-08-24 22:30:32 +0200

    GradeTooLowException class was modified

--- cpp_module05/ex00/GradeTooLowException.cpp
+++ cpp_module05/ex00/GradeTooLowException.cpp
@@ -1,17 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*    GradeTooLowException.cpp                          :+:      :+:    :+:   */
+/*   GradeTooLowException.cpp                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: melkholy <melkholy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 18:16:27 by melkholy          #+#    #+#             */
-/*   Updated: 2023/07/24 18:17:39 by melkholy         ###   ########.fr       */
+/*   Updated: 2023/08/24 20:11:52 by melkholy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "GradeTooLowException.hpp"
 
+GradeTooLowException::GradeTooLowException(std::string const& message)
+	: _message(message)
+{
+	this->_message.append("::GradeTooLowException");
+}
+
 const char* GradeTooLowException::what(void) const throw() {
-	return ("Bureaucrat::GradeTooLowException");
+	return (this->_message.c_str());
+}
+
+GradeTooLowException::~GradeTooLowException(void) _NOEXCEPT {
+	return;
 }
--- cpp_module05/ex00/GradeTooLowException.hpp
+++ cpp_module05/ex00/GradeTooLowException.hpp
@@ -6,11 +6,12 @@
 /*   By: melkholy <melkholy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 18:13:04 by melkholy          #+#    #+#             */
-/*   Updated: 2023/07/24 18:33:31 by melkholy         ###   ########.fr       */
+/*   Updated: 2023/08/24 20:07:43 by melkholy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include <string>
 #ifndef GRADETOOLOWEXCEPTION_CLASS
 #define GRADETOOLOWEXCEPTION_CLASS
 #include <iostream>
@@ -19,6 +20,11 @@
 class	GradeTooLowException: public std::exception
 {
 public:
+	GradeTooLowException(std::string const& message);
 	virtual const char*	what(void) const throw();
+	~GradeTooLowException(void) _NOEXCEPT;
+
+private:
+	std::string	_message;
 };
 #endif
